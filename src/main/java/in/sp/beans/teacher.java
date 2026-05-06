e09a31b4af11a1cc23e5180db90ad01183e30cde
Shubham2577 <shubhamlodhe2005@gmail.com>
2026-05-06 19:34:20 +0530

    to create the teaher class

---
+++ src/main/java/in/sp/beans/teacher.java
@@ -0,0 +1,24 @@
+package in.sp.beans;
+
+public class teacher 
+{
+
+	private int tno;
+	private String tname;
+	
+	public int getTno() {
+		return tno;
+	}
+	public void setTno(int tno) {
+		this.tno = tno;
+	}
+	public String getTname() {
+		return tname;
+	}
+	public void setTname(String tname) {
+		this.tname = tname;
+	}
+	
+	
+	
+}
